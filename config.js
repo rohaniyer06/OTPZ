f26cf916a8b2ab946a001b5eed65c1fabd8dfcbd
Rohan Iyer <rohaniyer06@gmail.com>
2025-08-11 21:07:24 -0700

    config for oauth/api

---
+++ config.js
@@ -0,0 +1,6 @@
+// Replace this after you create your OAuth client in Google Cloud Console.
+export const GOOGLE_CLIENT_ID = "137044696259-jkeiknfttbk5me5muekmsa3j6gmp9emm.apps.googleusercontent.com";
+
+// Scope: read-only access to Gmail.
+export const GMAIL_SCOPE = "https://www.googleapis.com/auth/gmail.readonly";
+
