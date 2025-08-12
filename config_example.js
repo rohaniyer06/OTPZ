3d7578b3354b043ee69abbf5ac7b8a23db7cb1ff
Rohan Iyer <rohaniyer06@gmail.com>
2025-08-11 21:18:22 -0700

    example config for oauth/gmail api

---
+++ config_example.js
@@ -0,0 +1,5 @@
+export const GOOGLE_CLIENT_ID = "OAUTH_CLIENT_ID.apps.googleusercontent.com";
+
+// Scope: read-only access to Gmail.
+export const GMAIL_SCOPE = "https://www.googleapis.com/auth/gmail.readonly";
+
