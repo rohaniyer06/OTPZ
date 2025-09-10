4d5ea82c35d1abea0224a18500dd4b1fec3ae4e8
Rohan Iyer <rohaniyer06@gmail.com>
2025-09-10 00:54:45 -0700

    chore: stop tracking config.js

--- config_example.js
+++
@@ -1,5 +0,0 @@
-export const GOOGLE_CLIENT_ID = "OAUTH_CLIENT_ID.apps.googleusercontent.com";
-
-// Scope: read-only access to Gmail.
-export const GMAIL_SCOPE = "https://www.googleapis.com/auth/gmail.readonly";
-
